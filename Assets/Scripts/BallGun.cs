35ee91928c5d0cd2da4fad52a217e33ae9d15032
Mirko <mirkoskroch@gmail.com>
2020-12-11 20:06:12 +0100

    Finished BouncyBallGun

---
+++ Assets/Scripts/BallGun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGun : MonoBehaviour
+{
+    public GameObject ballPrefab = null;
+    public float projectileSpeed = 20f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject projectile = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+            projectile.GetComponent<Rigidbody>().velocity = transform.forward * projectileSpeed;
+            Destroy(projectile, 10f);
+        }
+    }
+}

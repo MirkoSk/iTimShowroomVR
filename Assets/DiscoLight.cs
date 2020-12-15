b213e9955ab6abd2f5eaff1d5760e8f19c3ee898
Mirko <mirkoskroch@gmail.com>
2020-12-15 18:36:12 +0100

    lecture scripting

--- Assets/DiscoLight.cs
+++ Assets/DiscoLight.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DiscoLight : MonoBehaviour
 {
+    public float rotationSpeed = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class DiscoLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0f, 10f, 0f));
+        transform.Rotate(new Vector3(0f, rotationSpeed, 0f) * Time.deltaTime, Space.World);
     }
 }
